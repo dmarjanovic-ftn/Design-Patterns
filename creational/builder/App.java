8a0eeb17096a70229467ebf9c9fea16d2f9c0fba
dmarjanovic94 <dmarjanovic94@gmail.com>
2015-10-21 16:12:27 +0200

    Builder pattern initial commit.

---
+++ creational/builder/App.java
@@ -0,0 +1,20 @@
+package creational.builder;
+
+
+public class App {
+	
+	/* EXAMPLE: Document format conversion
+	 * 
+	 * 		Document can contain:
+	 * 			- header
+	 * 			- subheader
+	 * 			- paragraph
+	 * 			- list
+	 * 			- definition
+	 */
+
+	public static void main(String[] args) {
+
+	}
+
+}
